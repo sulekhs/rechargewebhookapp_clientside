ed862bdded69a5289f5e219b995857964beeb254
Sulekh <sulekhs@cybage.com>
2022-06-29 20:16:01 +0530

    RechargeEntryApp_ClientSide with React Material_UI V5

---
+++ rechargewebhookapp_clientside/src/pages/Home.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import RechargeForm from '../components/RechargeForm';
+import Navbar from './../components/Navbar';
+
+const Home = () => {
+  return (
+    <>
+      <Navbar />
+      <RechargeForm />
+    </>
+  )
+}
+
+export default Home
